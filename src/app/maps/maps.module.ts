93cebb032ab8994a4fae69f692898ee3ca465823
Andreea <andelenasamur@gmail.com>
2023-08-21 13:54:03 +0200

    mapbox installation

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import * as mapboxgl from 'mapbox-gl';
+(mapboxgl as any).accessToken = 'pk.eyJ1IjoiYW5kZWxlbmExOSIsImEiOiJjbGxrcGV1MmwwM244M2Rxc21iM2Q2cGw4In0.VejoOBF15sABEKmLSE7sHQ';
+
 import { MapsRoutingModule } from './maps-routing.module';
 import { MiniMapComponent } from './components/mini-map/mini-map.component';
 import { SideMenuComponent } from './components/side-menu/side-menu.component';
